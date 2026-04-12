419743c19fc3f2d00823e4ffa7e5f1b6247a9269
Parmod Kumar <parmod1feb2002@gmail.com>
2026-04-12 21:33:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ async function connectDB() {
 }
 
 connectDB();
-app.use(cors());
+app.use(cors({
+    origin:process.env.FRONTEND_URL
+}));
 app.use(express.json())
 
 app.use("/login",userRoute);
